4449bab5b015c697f055ff691edeba80fd9b626b
Fuzoka <unbeatablesites09@gmail.com>
2018-06-12 23:12:34 -0400

    Adding Color Game

---
+++ colorGame.js
@@ -0,0 +1,49 @@
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickcolor();
+var colorDisplay = document.querySelector("#colorDisplay");
+var messageDisplay = document.querySelector("#message")
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	// Add colors
+squares[i].style.background = colors[i];
+
+squares[i].addEventListener("click", function(){
+	var clickedColor = this.style.background;
+	if (clickedColor === pickedColor){
+		messageDisplay.textContent = "Correct";
+		changeColors(clickedColor);
+	}else{
+		this.style.background = "#232323";
+		messageDisplay.textContent = "Try Again";
+		}
+	});
+}
+
+function changeColors(color){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = color;
+	}
+}
+
+function pickcolor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+
+
+function generateRandomColors(num){
+	var arr = []
+	for(var i = 0; i < num; i++){
+	return arr;
+
+	function randomColor(){
+		var r = Math.floor(Math.random() * 256);
+		var g = Math.floor(Math.random() * 256);
+		var b = Math.floor(Math.random() * 256);
+	}
+}
